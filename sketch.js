e0554dc14585489203f113735ab2574b2e199866
rajdeep123von <59657573+rajdeep123von@users.noreply.github.com>
2020-10-16 16:41:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,207 +58,226 @@ function setup() {
   skygroup = new Group();
   cloudGroup = new Group();
 
-  skys1 = createSprite(56,height-490,400,20);
+  skys1 = createSprite(width-490,height-56,400,20);
   skys1.addImage("ground",sky);  
   skys1.scale = 1;
   skygroup.add(skys1);
 
-  skys2 = createSprite(skys1.x+112,height-490,400,20);
+  skys2 = createSprite(width-490,height-168,400,20);
   skys2.addImage("ground",sky);  
   skys2.scale = 1;
   skygroup.add(skys2);
 
-  skys3 = createSprite(skys2.x+112,height-490,400,20);
+  skys3 = createSprite(width-490,height-280,400,20);
   skys3.addImage("ground",sky);  
   skys3.scale = 1;
   skygroup.add(skys3);
 
-  skys4 = createSprite(skys3.x+112,height-490,400,20);
+  skys4 = createSprite(width-490,height-392,400,20);
   skys4.addImage("ground",sky);  
   skys4.scale = 1;
   skygroup.add(skys4);
 
-  skys5 = createSprite(skys4.x+112,height-490,400,20);
+  skys5 = createSprite(width-490,height-504,400,20);
   skys5.addImage("ground",sky);  
   skys5.scale = 1;
   skygroup.add(skys5);
 
-  skys6 = createSprite(skys5.x+112,height-490,400,20);
+  skys6 = createSprite(width-490,height-616,400,20);
   skys6.addImage("ground",sky);  
   skys6.scale = 1;
   skygroup.add(skys6);
 
-  skys7 = createSprite(skys6.x+112,height-490,400,20);
+  skys7 = createSprite(width-490,height-728,400,20);
   skys7.addImage("ground",sky);  
   skys7.scale = 1;
   skygroup.add(skys7);
 
-  skys8 = createSprite(skys7.x+112,height-490,400,20);
+  skys8 = createSprite(width-490,height-840,400,20);
   skys8.addImage("ground",sky);  
   skys8.scale = 1;
   skygroup.add(skys8);
 
-  skys9 = createSprite(skys8.x+112,height-490,400,20);
+  skys9 = createSprite(width-490,height-952,400,20);
   skys9.addImage("ground",sky);  
   skys9.scale = 1;
   skygroup.add(skys9);
 
-  skys10= createSprite(skys9.x+112,height-490,400,20);
+  skys10= createSprite(width-490,height-1064,400,20);
   skys10.addImage("ground",sky);  
   skys10.scale = 1;
   skygroup.add(skys10);
 
-  skys11 = createSprite(skys10.x+112,height-490,400,20);
+  skys11 = createSprite(width-490,height-1176,400,20);
   skys11.addImage("ground",sky);  
   skys11.scale = 1;
   skygroup.add(skys11);
 
-  skys12 = createSprite(skys11.x+112,height-490,400,20);
+  skys12 = createSprite(width-490,height-1288,400,20);
   skys12.addImage("ground",sky);  
   skys12.scale = 1;
   skygroup.add(skys12);
 
-  skys13 = createSprite(skys12.x+112,height-490,400,20);
+  skys13 = createSprite(width-490,height-1400,400,20);
   skys13.addImage("ground",sky);  
   skys13.scale = 1;
   skygroup.add(skys13);
 
-  skys14 = createSprite(skys13.x+112,height-490,400,20);
+  skys14 = createSprite(width-490,height-1512,400,20);
   skys14.addImage("ground",sky);  
   skys14.scale = 1;
   skygroup.add(skys14);
+  skygroup.setRotationEach(-90);
 
-  sea3 = createSprite(896/2,height+160,400,20);
+  sea3 = createSprite(width+160,height-896/2,400,20);
   sea3.addImage("ground",groundImage); 
-  sea3.velocityX = -7
+  sea3.velocityY = 7
   sea3.scale = 8;
+  sea3.rotation = -90;
 
-  sea2 = createSprite(sea3.x+896,height+160,400,20);
+  sea2 = createSprite(width+160,sea3.y+896,400,20);
   sea2.addImage("sea",groundImage);
-  sea2.velocityX = -7;
+  sea2.velocityY = 7;
   sea2.scale = 8;
+  sea2.rotation = -90;
 
-  sea1 = createSprite(sea2.x+896,height+160,400,20);
+  sea1 = createSprite(width+160,sea2.y+896,400,20);
   sea1.addImage("sea2",groundImage);
-  sea1.velocityX = -7;
+  sea1.velocityY = 7;
   sea1.scale = 8;
+  sea1.rotation = -90;
 
-  clouds1 = createSprite(270,height-340,400,20);
+  clouds1 = createSprite(width-340,height-270,400,20);
   clouds1.addImage("clouds",clouds);
   cloudGroup.add(clouds1);
 
-  clouds2 = createSprite(clouds1.x+clouds1.width,height-340,400,20);
+  clouds2 = createSprite(width-340,clouds1.y+clouds1.width,400,20);
   clouds2.addImage("clouds",clouds);
   cloudGroup.add(clouds2);
 
   
-  clouds3 = createSprite(clouds2.x+clouds2.width,height-340,400,20);
+  clouds3 = createSprite(width-340,clouds2.y+clouds2.width,400,20);
   clouds3.addImage("clouds",clouds);
   cloudGroup.add(clouds3);
+ 
 
+  clouds4 = createSprite(width-340,clouds3.y+clouds3.width,400,20);
+  clouds4.addImage("clouds",clouds);
+  cloudGroup.add(clouds4);
+ 
+
+  clouds5 = createSprite(width-340,clouds4.y+clouds4.width,400,20);
+  clouds5.addImage("clouds",clouds);
+  cloudGroup.add(clouds5);
+  cloudGroup.setRotationEach(-90);
   
-  farg = createSprite(width+30,height-250,400,20);
+  farg = createSprite(width-250,height+100,400,20);
   farg.addImage("sea",fg);
   farg.scale = 0.5;
-  farg.velocityX = -3;
-
+  farg.velocityY = 3;
+  farg.rotation = -90;
 
 
  
-  ship = createSprite(500,height-340);
+  ship = createSprite(width-340,height-500);
   ship.addImage( "running",ship_img);
   ship.scale = 0.4;
-  ship.velocityX = -3;
+  ship.velocityY = 3;
+  ship.rotation = -90;
   //ship.lifetime = 350;
   
-  plr = createSprite(350,height-150);
-  plr.addAnimation( "running",idle_img);
+  plr = createSprite(width-180,height-350);
+  plr.addAnimation( "running",run_img);
   plr.scale = 2;
+  plr.rotation = -90;
+ 
 
-  land10 = createSprite(1700,height-40);
+  land10 = createSprite(width-40,height-1700);
   land10.addImage( "running",big_img);
   land10.scale = 2.5 ;
   land10.setCollider("rectangle",0,0,52,55,0); 
-  land10.velocityX = -7;
+  land10.velocityY = 7;
   tilesGroup.add(land10);
 
-  land0 = createSprite(1570,height-40);
+  land0 = createSprite(width-40,height-1570);
   land0.addImage( "running",big_img);
   land0.scale = 2.5 ;
   land0.setCollider("rectangle",0,0,52,56,0); 
-  land0.velocityX = -7;
+  land0.velocityY = 7;
   tilesGroup.add(land0);
 
-  land9 = createSprite(1440,height-40);
+  land9 = createSprite(width-40,height-1440);
   land9.addImage( "running",big_img);
   land9.scale = 2.5 ;
   land9.setCollider("rectangle",0,0,52,56,0);
-  land9.velocityX = -7;
+  land9.velocityY = 7;
   tilesGroup.add(land9);
 
-  land8 = createSprite(1310,height-40);
+  land8 = createSprite(width-40,height-1310);
   land8.addImage( "running",big_img);
   land8.scale = 2.5 ;
   land8.setCollider("rectangle",0,0,52,56,0);
-  land8.velocityX = -7;
+  land8.velocityY = 7;
   tilesGroup.add(land8);
 
-  land7 = createSprite(1180,height-40);
+  land7 = createSprite(width-40,height-1180);
   land7.addImage( "running",big_img);
   land7.scale = 2.5 ;
   land7.setCollider("rectangle",0,0,52,56,0);
-  land7.velocityX = -7;
+  land7.velocityY = 7;
   tilesGroup.add(land7);
 
-  land6 = createSprite(1050,height-40);
+  land6 = createSprite(width-40,height-1050);
   land6.addImage( "running",big_img);
   land6.scale = 2.5 ;
   land6.setCollider("rectangle",0,0,52,56,0);
-  land6.velocityX = -7;
+  land6.velocityY = -7;
   tilesGroup.add(land6);
   
-  land5 = createSprite(790+130,height-40);
+  land5 = createSprite(width-40,height-(790+130));
   land5.addImage( "running",big_img);
   land5.scale = 2.5 ;
   land5.setCollider("rectangle",0,0,52,56,0);
-  land5.velocityX = -7;
+  land5.velocityY = 7;
   tilesGroup.add(land5);
 
-  land4 = createSprite(790,height-40);
+  land4 = createSprite(width-40,height-790);
   land4.addImage( "running",big_img);
   land4.scale = 2.5 ;
   land4.setCollider("rectangle",0,0,50,56,0);
-  land4.velocityX = -7;
+  land4.velocityY = 7;
   tilesGroup.add(land4);
 
-  land3 = createSprite(660,height-40);
+  land3 = createSprite(width-40,height-660);
   land3.addImage( "running",big_img);
   land3.scale = 2.5 ;
   land3.setCollider("rectangle",0,0,50,56,0);
-  land3.velocityX = -7;
+  land3.velocityY = 7;
   tilesGroup.add(land3);
 
-  land2 = createSprite(530,height-40);
+  land2 = createSprite(width-40,height-530);
   land2.addImage( "running",big_img);
   land2.scale = 2.5 ;
   land2.setCollider("rectangle",0,0,50,56,0);
-  land2.velocityX = -7;
+  land2.velocityY = 7;
   tilesGroup.add(land2);
 
-  land1 = createSprite(400,height-40);
+  land1 = createSprite(width-40,height-400);
   land1.addImage( "running",big_img);
   land1.scale = 2.5 ;
   land1.setCollider("rectangle",0,0,50,56,0);
-  land1.velocityX = -7;
+  land1.velocityY =7;
   tilesGroup.add(land1);
-  
+  tilesGroup.setRotationEach(-90);
 
   
-  gameover = createSprite(windowWidth/2,height-330,20,20);
+  gameover = createSprite(windowWidth-350,height/2,20,20);
   gameover.addImage(gameoverimage);
+  gameover.rotation = -90;
   
-  restart = createSprite(windowWidth/2,height-10,20,20);
+  restart = createSprite(windowWidth-40,height/2,20,20);
   restart.addImage(restartimage);
+  restart.rotation =-90;
 
   
   gameover.scale = 1;
@@ -270,9 +289,9 @@ function setup() {
  
   
   
-  title = createElement('h2');
+  /*title = createElement('h2');
   title.html("Pirate Runner");
-  title.position(windowWidth / 2 - 80,height-660);
+  title.position(windowWidth / 2 - 80,height-660);*/
   
   Button = createButton("Play Sound");
   Button.mousePressed(togglePlay);
@@ -291,7 +310,7 @@ if(gameState === PLAY){
       plr.addAnimation("run",run_img);
     }
     
-    if(birdsGroup.isTouching(plr)|| plr.y>height ||plr.x<0||objGroup.isTouching(plr)){
+    if(birdsGroup.isTouching(plr)|| plr.y>height ||plr.x>width||objGroup.isTouching(plr)){
      // plr.destroy();
       plr.visible = false;
      // bird.visibleEach = false;
@@ -300,15 +319,19 @@ if(gameState === PLAY){
       tiles2Group.setVisibleEach(false);
       tiles3Group.setVisibleEach(false);
       birdsGroup.setVisibleEach(false);
-      tilesGroup.setVelocityXEach(-1100);
-      tiles1Group.setVelocityXEach(-1100);
-      tiles2Group.setVelocityXEach(-1100);
-      tiles3Group.setVelocityXEach(-1100);
-      objGroup.setVelocityXEach(-1100);
-      birdsGroup.setVelocityXEach(-1100);
+      tilesGroup.setVelocityYEach(-1100);
+      tiles1Group.setVelocityYEach(-1100);
+      tiles2Group.setVelocityYEach(-1100);
+      tiles3Group.setVelocityYEach(-1100);
+      objGroup.setVelocityYEach(-1100);
+      birdsGroup.setVelocityYEach(-1100);
       gameState =END;
     }
-  if(keyDown( RIGHT_ARROW)){
+    if( (touches.length > 0) ) {
+     // plr.addImage( "running",jump_img);
+      plr.velocityX = -16 ;
+      touches =[];}
+  /*if(keyDown( RIGHT_ARROW)){a
     
     plr.addAnimation( "running",run_img);
     sea1.velocityX = -7
@@ -323,8 +346,8 @@ if(gameState === PLAY){
     birdsGroup.setVelocityXEach(-8);
    // birdsGroup.setLifetimeEach(500);
     farg.velocityX = -3;
-  }
-  if(keyDown( LEFT_ARROW)){
+  }*/
+  /*if(keyDown( LEFT_ARROW)){
     plr.addAnimation( "running",idle_img);
     sea1.velocityX = 0
     sea2.velocityX = 0
@@ -335,7 +358,7 @@ if(gameState === PLAY){
     tiles1Group.setVelocityXEach(0);
     /*tiles1Group.destroyEach();
     tiles2Group.destroyEach();
-    tiles3Group.destroyEach();*/
+    //tiles3Group.destroyEach();*
     birdsGroup.setVelocityXEach(0)
     //tilesGroup.destroyEach();
     tilesGroup.setLifetimeEach(-1);
@@ -343,8 +366,8 @@ if(gameState === PLAY){
     tiles2Group.setLifetimeEach(-1);
     tiles3Group.setLifetimeEach(-1);
     farg.velocityX = 0;
-   // bird.velocity = 0;
-  }
+   // bird.velocity = 
+  }*/
   /*if (plr.y>700||plr.x<0){
     gameState = END;
     //scoring
@@ -369,7 +392,7 @@ if(gameState === PLAY){
  
   
     //add gravity
-   plr.velocityY = plr.velocityY + 1.0;
+   plr.velocityX = plr.velocityX + 1.0;
     /* if(plr.y >tilesGroup.y-50 && plr.y < tilesGroup.y-40){
       plr.addImage( "running",land_img);
       }*/
@@ -399,16 +422,16 @@ if(gameState === PLAY){
    tiles3Group.setVisibleEach(false);
    birdsGroup.setVisibleEach(false);
    objGroup.setVisibleEach(false);
-   tilesGroup.setVelocityXEach(-1100);
-   tiles1Group.setVelocityXEach(-1100);
-   tiles2Group.setVelocityXEach(-1100);
-   tiles3Group.setVelocityXEach(-1100);
+   tilesGroup.setVelocityYEach(-1100);
+   tiles1Group.setVelocityYEach(-1100);
+   tiles2Group.setVelocityYEach(-1100);
+   tiles3Group.setVelocityYEach(-1100);
 tilesGroup.setLifetimeEach(-1);
 birdsGroup.setLifetimeEach(-1);
-    sea1.velocityX = 0;
-    sea2.velocityX = 0;
-    sea3.velocityX = 0;
-    farg.velocityX = 0;
+    sea1.velocityY = 0;
+    sea2.velocityY = 0;
+    sea3.velocityY = 0;
+    farg.velocityY = 0;
     
     //bird.destroy();
   
@@ -432,9 +455,9 @@ console.log(frameCount);
 
 }
 function keyPressed(){
-  if(keyCode===32 || (touches.length > 0) ) {
+  if(keyCode===32) {
   plr.addImage( "running",jump_img);
-  plr.velocityY = -16 ;
+  plr.velocityX = -16 ;
   touches =[];
   //playSound("jump.mp3");
 }}
@@ -448,31 +471,31 @@ function keyPressed(){
 function spawntiles() {
   //write code here to spawn the clouds
   if (frameCount% 79 === 0 ) {
-    var tiles = createSprite(width+50 ,height-250,40,10);
+    var tiles = createSprite(width+50 ,-(height)+50,40,10);
    
-    tiles.y = Math.round(random(height-310,height-40));
+    tiles.x = Math.round(random(width-310,width-40));
     tiles.addImage("tile",tile);
     tiles.scale = 2.5;
     tiles.setCollider("rectangle",0,0,60,37,0);   
-    tiles.velocityX = -7;
+    tiles.velocityY = 7;
 
-    var tiles1 =  createSprite(width+150,tiles.y,40,10);
+    var tiles1 =  createSprite(tiles.x,-(height)+150,40,10);
     tiles1.addImage("tile",tile);
     tiles1.scale = 2.5;
     tiles1.setCollider("rectangle",0,0,60,37,0);
-  tiles1.velocityX = -7;
+  tiles1.velocityY = 7;
 
-    var tiles2 =  createSprite(width+250,tiles.y,40,10);
+    var tiles2 =  createSprite(tiles.x,-(height)+250,40,10);
     tiles2.addImage("tile",tile);
     tiles2.scale = 2.5;
     tiles2.setCollider("rectangle",0,0,60,37,0);
-   tiles2.velocityX = -7;
+   tiles2.velocityY =7;
 
-   var tiles3 =  createSprite(width+350,tiles.y,40,10);
+   var tiles3 =  createSprite(tiles.x,-(height)+350,40,10);
     tiles3.addImage("tile",tile);
     tiles3.scale = 2.5;
     tiles3.setCollider("rectangle",0,0,60,37,0);
-   tiles3.velocityX = -7
+   tiles3.velocityY = 7
 
      //assign lifetime to the variable
     tiles.lifetime = 500;
@@ -488,14 +511,18 @@ function spawntiles() {
     tiles1Group.add(tiles1);
     tiles2Group.add(tiles2);
     tiles3Group.add(tiles3);
-    
+
+    tiles1Group.setRotationEach(-90);
+    tiles2Group.setRotationEach(-90);
+    tiles3Group.setRotationEach(-90);
+    tilesGroup.setRotationEach(-90);
   }
   if(frameCount% 316=== 0 && score>400){
-    var spike = createSprite(width+150,tiles.y-30,20,20);
+    var spike = createSprite(tiles.x-30,-(height)+150,20,20);
     spike.addImage("run",spike_img);
     spike.scale = 0.23;
     spike.lifetime = 500;
-    spike.velocityX = -7;
+    spike.velocityY = 7;
     spike.setCollider("rectangle",0,0,200,200,0);
     //spike.debug = true;
     spike.collide(tilesGroup);
@@ -503,7 +530,7 @@ function spawntiles() {
     spike.collide(tiles2Group);
     spike.collide(tiles3Group);
     objGroup.add(spike);
-   
+    objGroup.setRotationEach(-90);   
 }
 
 }
@@ -522,39 +549,39 @@ function spawntiles() {
   }}*/
   function spawnbirds(){
     if(frameCount% 156=== 0){
-      var bird = createSprite(width+50,height-540,20,20);
+      var bird = createSprite(width+50,-(height)+50,20,20);
       bird.addAnimation("flying",bird_img);
-      bird.y = Math.round(random(height-540,height-240));
-      bird.velocityX = -8;
+      bird.x = Math.round(random(width-400,width-100));
+      bird.velocityY = 8;
       bird.scale=0.8;
       bird.setCollider("rectangle",0,0,50,20,0);
      // bird.debug = true;
       bird.lifetime = 500;
       birdsGroup.add(bird);
-    
+      birdsGroup.setRotationEach(-90);    
   }
   
 }
 function sea(){
-  if(sea3.x<=-896/2 && sea1.x===(896/2)*3){      
-    sea3.x=sea1.x+896
-    sea3.velocityX = -7;
+  if(sea3.y<=-896/2 && sea1.y===(896/2)*3){      
+    sea3.y=sea1.y+896
+    sea3.velocityY = 7;
   }
    
   
-  if(sea2.x<=-896/2){
-    sea2.x=sea3.x+896
-   sea2.velocityX = -7;
+  if(sea2.y<=-896/2){
+    sea2.y=sea3.y+896
+   sea2.velocityY = 7;
   }
     
-  if(sea1.x<=-896/2){
-    sea1.x=sea2.x+896
-   sea1.velocityX = -7;
+  if(sea1.y<=-896/2){
+    sea1.y=sea2.y+896
+   sea1.velocityY = 7;
   }
 }
 function farground(){
-  if(farg.x<=-300){
-    farg.x=width+100;
+  if(farg.y<=-300){
+    farg.y=height-height+100;
   }}
 
 
@@ -567,12 +594,12 @@ function reset() {
   gameover.visible = false;
   restart.visible = false;
 
-  tilesGroup.setVelocityXEach(-7);
-  tiles1Group.setVelocityXEach(-7);
-  tiles2Group.setVelocityXEach(-7);
-  tiles3Group.setVelocityXEach(-7);
-  objGroup.setVelocityXEach(-7);
-  birdsGroup.setVelocityXEach(-7);
+  tilesGroup.setVelocityYEach(7);
+  tiles1Group.setVelocityYEach(7);
+  tiles2Group.setVelocityYEach(7);
+  tiles3Group.setVelocityYEach(7);
+  objGroup.setVelocityYEach(7);
+  birdsGroup.setVelocityYEach(7);
 
   tilesGroup.setVisibleEach(true);
   tiles1Group.setVisibleEach(true);
@@ -580,20 +607,21 @@ function reset() {
   tiles3Group.setVisibleEach(true);
   birdsGroup.setVisibleEach(true);
   objGroup.setVisibleEach(true);
-  plr.x = 350;
-  plr.velocityX = 0;
-  plr.y = height-150;
-  ship.x = 500;
-  land1.x = 400;
-  land2.x = 530;
-  land3.x = 660;
-  land4.x = 790;
-  land5.x = 920;
-  land6.x = 1050;
-  land7.x = 1180;
-  land8.x = 1310;
-  land9.x = 1440;
-  land0.x = 1570;
+  plr.x = width-180;
+  plr.velocityY = 0;
+  plr.y = height-350;
+  ship.y = height-500;
+  land1.y = height-400;
+  land2.y = height-530;
+  land3.y = height-660;
+  land4.y = height-790;
+  land5.y = height-920;
+  land6.y = height-1050;
+  land7.y = height-1180;
+  land8.y = height-1310;
+  land9.y = height-1440;
+  land0.y = height-1570;
+  land10.y = height-1700;
   //music1.play();
  // tilesa.x = 1000;
   
